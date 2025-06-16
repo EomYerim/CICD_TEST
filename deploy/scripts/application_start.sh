6a3c363d336f86f784507fd5cf63522065f312cb
Eom-Ye-rim <dpfladlwkdrn@naver.com>
2025-06-16 20:28:50 +0900

    fix script v3

--- deploy/scripts/application_start.sh
+++ deploy/scripts/application_start.sh
@@ -3,5 +3,4 @@ echo "[ApplicationStart] Spring Boot 앱 실행"
 
 cd /home/ubuntu/app
 
-# 백그라운드 실행, 로그는 절대 경로로 지정
-nohup java -jar app.jar > /home/ubuntu/app/app.log 2>&1 &
+nohup java -jar app.jar > app.log 2>&1 &
