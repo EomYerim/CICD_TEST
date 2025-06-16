4cbe3fe469e8781ef9848aeb7ce7dc2a93392c00
Eom-Ye-rim <dpfladlwkdrn@naver.com>
2025-06-16 20:08:05 +0900

    update scripts file

--- deploy/scripts/after_install.sh
+++ deploy/scripts/after_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "[AfterInstall] 권한 설정 및 기타 작업"
 
+# jar 실행 권한 부여 (보통 없어도 되지만 명시적으로)
 chmod +x /home/ubuntu/app/app.jar
--- deploy/scripts/application_start.sh
+++ deploy/scripts/application_start.sh
@@ -3,5 +3,8 @@ echo "[ApplicationStart] Spring Boot 앱 실행"
 
 cd /home/ubuntu/app
 
-# 백그라운드로 실행하고 로그는 app.log에 저장
+# 기존 프로세스 종료 (안전하게 처리)
+pkill -f 'java -jar' || echo "실행 중인 프로세스 없음"
+
+# 백그라운드 실행 및 로그 저장
 nohup java -jar app.jar > app.log 2>&1 &
